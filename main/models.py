8489adff4a2ffcd35d1b1090ca91c5b6086a3d78
Lessyarta Kamali Sopamena Pirade <190507046+kamali-pirade@users.noreply.github.com>
2025-09-09 15:41:27 +0700

    Membuat aplikasi main, membuat model, mengisi views.py, routing urls.py.

---
+++ main/models.py
@@ -0,0 +1,35 @@
+import uuid
+from django.db import models
+
+class News(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    # Fitur wajib
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+
+    # Fitur tambahan untuk Garuda Gear
+    stock = models.IntegerField()
+    brand = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
