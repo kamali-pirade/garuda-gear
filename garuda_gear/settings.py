cd9c91252799a41f06ad9365493a6a7929064416
Lessyarta Kamali Sopamena Pirade <190507046+kamali-pirade@users.noreply.github.com>
2025-09-29 20:34:50 +0700

    Kembali ke kode awal

--- garuda_gear/settings.py
+++ garuda_gear/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_t-$e-@hph1)g)0jabt&bcgul8n&^ni#gpgev=^-_v$l6l7lgz
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lessyarta-kamali-garudagear.pbp.cs.ui.ac.id"]
 
@@ -145,11 +145,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
